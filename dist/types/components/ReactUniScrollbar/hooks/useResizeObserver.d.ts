54c411969e6f9f6e61ff7df48a364e26c917bb4e
wujimalayo <597605153@qq.com>
2025-04-14 21:29:51 +0800

    更新 .gitignore 并清理缓存

--- dist/types/components/ReactUniScrollbar/hooks/useResizeObserver.d.ts
+++
@@ -1,6 +0,0 @@
-import { RefObject } from "react";
-declare const useResizeObserver: (elementRef: RefObject<HTMLElement | null>) => {
-    width: number;
-    height: number;
-};
-export default useResizeObserver;
